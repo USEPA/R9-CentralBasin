1cd6796e1d8f4ee9c1fa69f7279cefe0d7c8a6f2
Travis Bock <tbock@innovateteam.com>
2021-07-02 11:23:49 -0700

    add another test

--- src/widgets.spec.ts
+++ src/widgets.spec.ts
@@ -1,4 +1,4 @@
-import {initFeatureTable, initWidgets, toggleFeatureTable} from './widgets';
+import { initFeatureTable, initTimeSlider, initWidgets, toggleFeatureTable } from './widgets';
 
 jest.mock('@arcgis/core/widgets/LayerList');
 jest.mock('@arcgis/core/widgets/Legend');
@@ -6,37 +6,44 @@ jest.mock('@arcgis/core/widgets/Legend');
 let spy: jest.SpyInstance;
 
 describe('widgets', () => {
-    beforeEach(() => {
-        spy = jest.spyOn(document, 'getElementById');
-        const mockElem: any = {
-            offsetWidth: 300,
-        };
-        spy.mockReturnValue(mockElem);
-    });
+	const widgets: any[] = [];
+	const view: any = {
+		ui: {
+			add(w: any) {
+				widgets.push(w);
+			},
+		},
+		watch() {},
+	};
 
-    it('initializes widgets in view', () => {
-        const widgets: any[] = [];
-        const view: any = {
-            ui: {
-                add(w: any) {
-                    widgets.push(w);
-                },
-            },
-            watch() {
-            },
-        };
-        initWidgets(view);
-        expect(widgets).toHaveLength(8);
-    });
+	beforeEach(() => {
+		spy = jest.spyOn(document, 'getElementById');
+		const mockElem: any = {
+			offsetWidth: 300,
+		};
+		spy.mockReturnValue(mockElem);
+	});
+
+	it('initializes widgets in view', () => {
+		const widgetsLength = widgets.length;
+		initWidgets(view);
+		expect(widgets).toHaveLength(widgetsLength + 8);
+	});
+
+	it('initializes timeSlider', () => {
+		const widgetsLength = widgets.length;
+		initTimeSlider(view);
+		expect(widgets).toHaveLength(widgetsLength + 1);
+	});
 });
 
 describe('featureTable', () => {
-    let spyCreate: jest.SpyInstance;
-    let spyRemove: jest.SpyInstance;
-    let spyAdd: jest.SpyInstance;
-    let mockElement: HTMLElement;
-    beforeAll(() => {
-        spyCreate = jest.spyOn(document, 'getElementById');
+	let spyCreate: jest.SpyInstance;
+	let spyRemove: jest.SpyInstance;
+	let spyAdd: jest.SpyInstance;
+	let mockElement: HTMLElement;
+	beforeAll(() => {
+		spyCreate = jest.spyOn(document, 'getElementById');
 		mockElement = document.createElement('appContainer');
 		spyRemove = jest.spyOn(mockElement, 'removeChild');
 		spyRemove.mockReturnValue(null);
@@ -44,15 +51,15 @@ describe('featureTable', () => {
 		spyAdd.mockReturnValue(null);
 		spyCreate.mockReturnValue(mockElement);
 		initFeatureTable({} as any);
-    })
-    it('removes table and toggles label when not checked', () => {
-    	const e = {checked: false} as any;
-        toggleFeatureTable(e);
-        expect(spyRemove).toHaveBeenCalled();
-    });
-    it('add table and toggles label when not checked', () => {
-    	const e = {checked: true} as any;
-        toggleFeatureTable(e);
-        expect(spyRemove).toHaveBeenCalled();
-    });
+	});
+	it('removes table and toggles label when not checked', () => {
+		const e = { checked: false } as any;
+		toggleFeatureTable(e);
+		expect(spyRemove).toHaveBeenCalled();
+	});
+	it('add table and toggles label when not checked', () => {
+		const e = { checked: true } as any;
+		toggleFeatureTable(e);
+		expect(spyRemove).toHaveBeenCalled();
+	});
 });
