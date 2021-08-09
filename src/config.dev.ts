1003d10393c082a3dbe8923f8f30ff9c38c7f8c4
Brads <bradleyjsnider@gmail.com>
2021-08-09 16:15:17 -0700

    updated dev appId with prod appid

--- src/config.dev.ts
+++ src/config.dev.ts
@@ -144,7 +144,7 @@ const gamaFields = [
 
 export const configDev = {
 	portalEnv: {
-		appId: 'ZtlpDht9ywRCA4Iq',
+		appId: 'RjgBsWrJbfY8hMGY',
 		portalUrl: 'https://epa.maps.arcgis.com',
 		blankBasemap: 'c0af3abd0d60427ba659e38d457fbe07',
 		// webScene: 'dd370d1e2c194f4491078b579379f1d1',
